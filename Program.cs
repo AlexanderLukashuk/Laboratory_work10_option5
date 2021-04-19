31036ad9d023e204c14cd3afdf70eb4db3523474
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-19 13:43:51 +0600

    Первое создание

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tast10var5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
